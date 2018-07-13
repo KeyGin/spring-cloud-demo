eb911e0642977ee02d0bc7cd0b77b11539549431
xavier <675260780@qq.com>
2018-07-13 12:08:44 +0800

    change client config

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -2,11 +2,20 @@ package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class 	ConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String home(){
+	    return "Hello World!";
+    }
+
 }
